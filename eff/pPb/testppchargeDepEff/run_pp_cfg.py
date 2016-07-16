0cf3d1d145c4ec8aa8e5c5917d610ee37776d26f
Shengquan Tuo <shengquan.tuo@cern.ch>
2016-07-15 23:48:05 -0500

    add positve and negative particle efficiency calculation options

---
+++ eff/pPb/testppchargeDepEff/run_pp_cfg.py
@@ -0,0 +1,95 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('TRACKANA')
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.ReconstructionHeavyIons_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('TrackingCode.HIRun2015Ana.HITrackCorrectionAnalyzer_cfi')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('trk.root')
+)
+
+process.load("SimTracker.TrackAssociation.trackingParticleRecoTrackAsssociation_cfi")
+
+process.tpRecoAssocGeneralTracks = process.trackingParticleRecoTrackAsssociation.clone()
+process.tpRecoAssocGeneralTracks.label_tr = cms.InputTag("generalTracks")
+
+process.load("SimTracker.TrackAssociatorProducers.quickTrackAssociatorByHits_cfi")
+process.quickTrackAssociatorByHits.SimToRecoDenominator = cms.string('reco')
+
+process.load("SimTracker.TrackerHitAssociation.clusterTpAssociationProducer_cfi")
+
+# Input source
+process.source = cms.Source("PoolSource",
+    duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
+    fileNames =  cms.untracked.vstring(
+##pythia
+#'/store/user/velicanu/MinBias_TuneCUETP8M1_5p02TeV-pythia8/MinBias_TuneCUETP8M1_5p02TeV_pythia8_pp502Fall15_MCRUN2_71_V1_ppreco_RECODEBUG_v0/151117_131129/0000/step3_100.root'
+'/store/user/dgulhan/PYTHIA_QCD_TuneCUETP8M1_cfi_GEN_SIM_5020GeV/PYTHIA_QCD80_TuneCUETP8M1_cfi_RECODEBUGpp_757p1_timeslew_HcalRespCorrs_v4_00_mc/151211_111516/0000/step3_1.root'
+)
+)
+### Track cuts ###
+# input collections
+process.HITrackCorrections.centralitySrc = cms.InputTag("centralityBin","HFtowers")
+process.HITrackCorrections.trackSrc = cms.InputTag("generalTracks")
+process.HITrackCorrections.vertexSrc = cms.InputTag("offlinePrimaryVertices")
+process.HITrackCorrections.qualityString = cms.string("highPurity")
+process.HITrackCorrections.pfCandSrc = cms.InputTag("particleFlow")
+process.HITrackCorrections.jetSrc = cms.InputTag("ak4CaloJets")
+# options
+process.HITrackCorrections.useCentrality = False
+process.HITrackCorrections.applyTrackCuts = True
+process.HITrackCorrections.fillNTuples = False
+process.HITrackCorrections.applyVertexZCut = True
+process.HITrackCorrections.doVtxReweighting = False
+process.HITrackCorrections.doCaloMatched = True
+# cut values
+process.HITrackCorrections.dxyErrMax = 3.0
+process.HITrackCorrections.dzErrMax = 3.0
+process.HITrackCorrections.ptErrMax = 0.3 #track trigger 0.1
+process.HITrackCorrections.nhitsMin = 0 # track trigger 11
+process.HITrackCorrections.chi2nMax = 9999.9 # track trigger 0.15
+process.HITrackCorrections.reso = 0.2
+#process.HITrackCorrections.crossSection = 1.0 #1.0 is no reweigh
+# vertex reweight parameters
+#pp 5TeV
+process.HITrackCorrections.vtxWeightParameters = cms.vdouble(0.0028637,0.225207,5.61457,0.00296148,0.640783,5.41939)
+###
+process.HITrackCorrections.algoParameters = cms.vint32(4,5,6,7,8)
+
+## pos, neg, all
+process.HITrackCorrections.partileCharge = cms.int32(2)
+process.HITrackCorrectionsPos = process.HITrackCorrections.clone()
+process.HITrackCorrectionsPos.partileCharge = cms.int32(1)
+process.HITrackCorrectionsNeg = process.HITrackCorrections.clone()
+process.HITrackCorrectionsNeg.partileCharge = cms.int32(-1)
+
+process.p = cms.Path(
+                      process.tpClusterProducer *
+                      process.quickTrackAssociatorByHits *
+                      process.tpRecoAssocGeneralTracks *
+                      process.HITrackCorrections
+)
+process.p2 = cms.Path(
+                      process.tpClusterProducer *
+                      process.quickTrackAssociatorByHits *
+                      process.tpRecoAssocGeneralTracks *
+                      process.HITrackCorrectionsPos
+)
+process.p3 = cms.Path(
+                      process.tpClusterProducer *
+                      process.quickTrackAssociatorByHits *
+                      process.tpRecoAssocGeneralTracks *
+                      process.HITrackCorrectionsNeg
+)
